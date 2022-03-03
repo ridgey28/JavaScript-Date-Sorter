f0f54d68c55f8e9c8021feed012e771710e584f2
Tracy Ridge <tracylridge@gmail.com~>
2022-03-03 22:06:33 +0000

    More tweaks - added age column and regen button

--- js/global.js
+++ js/global.js
@@ -28,15 +28,16 @@ window.onload = () => {
 			tblBody = document.createElement("tbody"),
 			tblHeader = document.createElement("thead");
 
-		table.classList.add("table");
-		tblHeader.innerHTML = "<tr><td>Name</td><td>Birth Date</td></tr>";
+		table.classList.add("sortable");
+		tblHeader.innerHTML =
+			"<tr><th>Name</th><th>Birth Date</th><th>Age</th></tr>";
 		table.appendChild(tblHeader);
 		list.forEach((element) => {
-			console.log(typeof element);
 			let row = document.createElement("tr");
 			for (let i = 0; i < element.length; i++) {
 				let cell = document.createElement("td"),
 					cellText = document.createTextNode(element[i]);
+
 				cell.appendChild(cellText);
 				row.appendChild(cell);
 			}
@@ -58,12 +59,49 @@ const handleSubmit = (e) => {
 
 	let arr = [],
 		data = [...e.currentTarget.elements].filter((ele) => ele.type !== "submit");
+
+	const dob = new Date(data[1].value);
+	const age = CalculateAge(dob);
 	data.forEach((element) => {
 		arr.push(element.value);
 	});
+	arr.push(age);
 	list.push(arr);
 	localStorage.setItem("JSListDate", JSON.stringify(list));
+	location.reload();
 };
 document.getElementById("userData").addEventListener("submit", handleSubmit);
 
 //Calculate age
+let CalculateAge = (dob) => {
+	const currentDate = new Date();
+	// To calculate the time difference of two dates
+	const Difference_In_Time = currentDate.getTime() - dob.getTime();
+	// To calculate the no. of days between two dates
+	return Math.floor(Difference_In_Time / (1000 * 3600 * 24) / 365);
+};
+
+/**
+ * Check age will recalculate the age in the database
+ *
+ * @param   {[type]}  checkAge  [checkAge description]
+ * @param   {[type]}  click     [click description]
+ *
+ * @return  {[type]}            [return description]
+ */
+document.getElementById("checkAge").addEventListener("click", () => {
+	if (localStorage.getItem("JSListDate")) {
+		data = localStorage.getItem("JSListDate");
+		list = JSON.parse(data);
+		console.log(list);
+		list.forEach((element) => {
+			console.log(element);
+			const dob = new Date(element[1]);
+			element[2] = CalculateAge(dob);
+		});
+		localStorage.setItem("JSListDate", JSON.stringify(list));
+		location.reload();
+	} else {
+		//display error message
+	}
+});
